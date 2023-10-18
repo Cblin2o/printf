924923b78a4e441f68b2832978beefd5f05bb8a9
Cblin2o <kolawolerasheed59@gmail.com>
2023-10-17 18:10:55 -0700

    initial

--- print_bin.c
+++ print_bin.c
@@ -10,11 +10,11 @@ int print_bin(va_list val)
 {
 	int flag = 0;
 	int count = 0;
-	int 1, a = 1, b;
+	int i, a = 1, b;
 	unsigned int num = va_arg(val, unsigned int);
 	unsigned int p;
 
-	for (1 = 0; i < 32; i++)
+	for (i = 0; i < 32; i++)
 	{
 		p = ((a << (32 - i)) & num);
 		if (a >> (31 - i))
