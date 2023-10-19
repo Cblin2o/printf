6c5f32aeb5e04bbd7be99574f1597ac06586e5cd
K-tosh <ericmusanyi8@gmail.com>
2023-10-19 07:15:07 -0700

    initial

--- utils.c
+++ utils.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
@@ -83,4 +84,3 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
-
